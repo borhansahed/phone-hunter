463038991fc82b3828ae4aed4aa57452efc2683d
wahi90 <mdshahed1098@gmail.com>
2022-03-01 12:39:24 +0600

    work on phonedetails

--- mobile.js
+++ mobile.js
@@ -7,25 +7,44 @@ fetch(url)
 .then(res => res.json())
 .then(data => displayMobile(data.data))
 }
-const displayMobile = phone => {
+const displayMobile = phones => {
+ 
    const div = document.getElementById('card-container');
-    phone.forEach(items => {
+   div.textContent='';
+    phones.forEach(items => {
+      
         const CardDiv = document.createElement('div')
         CardDiv.classList.add('phone-card')
         CardDiv.innerHTML=`
         <div class="col  w-75 h-100  mx-auto">
-        <div class="card bg-light">
-          <img src="${items.image}" class="card-img-top  mt-3 mx-auto" alt="...">
-          <div class="card-body ms-2">
+        <div class="card bg-dark shadow-lg text-white">
+          <img src="${items.image}" class="card-img-top  mt-3 mx-auto " alt="...">
+          <div class="card-body ms-3">
             <h5 class="card-title">${items.brand}</h5>
            <h6>${items.phone_name}</h6>
-           <button class="btn btn-outline-secondary">Show More</button>
+           <button onclick="searchPhoneDetails('${items.slug}')" class="btn btn-outline-secondary">Show More</button>
           </div>
         </div>
       </div>
      ` 
      div.appendChild(CardDiv);
      
-        console.log(items)
+      
     })
+}
+
+const searchPhoneDetails = id => {
+  // console.log(id);
+ const idUrl =`https://openapi.programming-hero.com/api/phone/${id}`;
+ fetch(idUrl)
+ .then(res =>res.json())
+ .then(data => displayPhoneDetails(data.data))
+}
+
+const displayPhoneDetails = phone => {
+  console.log(phone)
+  for(const item in phone){
+    console.log(item)
+  }
+ 
 }
