29c9e36d444779331baa3cc73ebe04b174b32e81
wahi90 <mdshahed1098@gmail.com>
2022-03-02 13:05:30 +0600

    no results section

--- js/mobile.js
+++ js/mobile.js
@@ -5,13 +5,18 @@
 const searchInput = document.getElementById('search-input');const searchText = searchInput.value;
 
 searchInput.value ='';
+
 const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+
+
 fetch(url)
 .then(res => res.json())
-.then(data => displayMobile(data.data))
+.then(data => displayMobile(data.data.slice(0,20)))
 }
 const displayMobile = phones => {
- 
+  console.log(phones)
+ if(!phones[0]){
+  return alert('not found');}
    const div = document.getElementById('card-container');
    div.textContent='';
     phones.forEach(items => {
@@ -20,21 +25,24 @@ const displayMobile = phones => {
         CardDiv.classList.add('phone-card')
         CardDiv.innerHTML=`
         <div class="col  w-75 h-100  mx-auto">
-        <div class="card bg-dark shadow-lg text-white">
+         <div class="card bg-dark shadow-lg text-white">
           <img src="${items.image}" class="card-img-top  mt-3 mx-auto " alt="...">
           <div class="card-body ms-3">
             <h5 class="card-title">${items.phone_name}</h5>
            <h6>${items.brand}</h6>
            <button onclick="searchPhoneDetails('${items.slug}')" class="btn btn-outline-secondary" id="#modal-js">Show Details</button>
           </div>
-        </div>
+         </div>
+        
       </div>
      ` 
      div.appendChild(CardDiv);
      
       
     })
+    const utton = document.createElement('button')
 }
+
 
 const searchPhoneDetails = id => {
   // console.log(id);
@@ -45,15 +53,18 @@ const searchPhoneDetails = id => {
 }
 
 const displayPhoneDetails = phone => {
-  console.log(phone)
+  
   const features =phone.mainFeatures;
-  console.log(features)
+ 
   const sensor = features.sensors;
   const modalDiv =document.getElementById('modal-js')
- 
+  
  modalDiv.textContent='';
+ 
   const newModalDiv =document.createElement('div')
+  newModalDiv.innerHTML='';
   newModalDiv.classList.add('phone-details')
+  
   newModalDiv.innerHTML=
   ` <div class="row d-flex  align-items-center   bg-dark shadow-lg text-white ">
               <div class="col-5 ">
@@ -63,7 +74,7 @@ const displayPhoneDetails = phone => {
                 <div class="card-body ms-3">
                  
                 <h5></h5>
-                <p>${phone.releaseDate}</p>
+                <p>${phone.releaseDate ? phone.releaseDate:'Coming soon'}</p>
                 <button class="btn btn-outline-secondary" data-bs-toggle="modal"  data-bs-target="#mymodal">Buy Now</button>
                 
                 </div>
@@ -72,7 +83,7 @@ const displayPhoneDetails = phone => {
               <div class="col-md-7  text-white">
               
                 
-                <div class="card-body ">
+                <div class="card-body overflow-hidden ">
 
                
                  <h5> Specifications </h5>
@@ -82,12 +93,12 @@ const displayPhoneDetails = phone => {
                  <p><span>.DisplaySize :</span> ${features.displaySize}</p>
                  <p><span>.Memory :</span> ${features.memory}</p>
                  <p><span>.sensors</span> : ${features.sensors}</p>
-                 <p>.Bluetooh : ${phone.others.Bluetooth}</p>
-                 <p>.GPS : ${phone.others.GPS}</p>
-                 <p>.USB : ${phone.others.USB}</p>
-                 <p>.Radio : ${phone.others.Radio}</p>
-                 <p>.NFC : ${phone.others.NFC}</p>
-                 <p>.WLAN : ${phone.others.WLAN}</p>
+                 <p>.Bluetooh : ${phone.others?.Bluetooth ?phone.others.Bluetooth:'No'}</p>
+                 <p>.GPS : ${phone.others?.GPS ?phone.others.GPS:'No' }</p>
+                 <p>.USB : ${phone.others?.USB ?phone.others.USB:'No'}</p>
+                 <p>.Radio : ${phone.others?.Radio ? phone.others.Radio:'No'}</p>
+                 <p>.NFC : ${phone.others?.NFC ?phone.others.NFC:'No'}</p>
+                 <p>.WLAN : ${phone.others?.WLAN ?phone.others.WLAN:'No'}</p>
                     
                  
                 
@@ -100,18 +111,6 @@ const displayPhoneDetails = phone => {
 
 `
 
-                
-              
-              
-                
-                
-               
-                
-                 
-                 
-                 
-               
-               
 modalDiv.appendChild(newModalDiv);
  
   
@@ -119,4 +118,15 @@ modalDiv.appendChild(newModalDiv);
  
 
  
-}
+}                
+              
+              
+                
+                
+               
+                
+                 
+                 
+                 
+               
+               
